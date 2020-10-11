ddd3bdef917622f445ea8d797e5c6769fc68467c
Andres Rojas <andres.rojas.isaza@gmail.com>
2020-10-11 12:39:37 -0500

    Added logic to parse URLs and extract the path using the WHATWG URL API

--- index.js
+++ index.js
@@ -4,13 +4,26 @@
 
 // Global variables for the server
 let port = 3000;
+const serverHost = 'http://localhost';
 
 // Dependencies
 const http = require('http');
+const URL = require('url').URL;
 
 // Respond to all requests with a String
 const server = http.createServer((req, res) => {
+    // Get the URL and parse it
+    const parsedUrl = new URL(req.url, `${serverHost}:${port}`);
+
+    // Get the path from the URL
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'').trim()
+
+    // Send the response
     res.end("hello World! \n");
+
+    // Log the request URI
+    console.log(`Request received on path: ${trimmedPath === "" ? "[home]": trimmedPath}`);
 });
 
 // Start the server and Listen on port 3000
